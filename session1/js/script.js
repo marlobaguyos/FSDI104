a8cc0c720d9e53a8dbcb941414d2944cedd6d9ee
Marlo <marlo.baguyos@outlook.com>
2020-02-11 20:59:15 -0500

    update

--- session1/js/script.js
+++ session1/js/script.js
@@ -1,1 +1,57 @@
-alert("hello")
+//alert("hello")
+
+//prompt("Enter your name: ");
+console.log("my first message on the console");
+
+var x=10;
+let y=20;
+const name="Chris";
+
+console.log(name);
+x="hello";
+console.log(x);
+y = 100.70;
+console.log(y);
+
+var sentence = 'Cohort 7';
+
+console.log(sentence);
+sentence = "the best cohort is " + sentence;
+sentence=sentence.replace('best', 'worst');
+
+console.log(sentence);
+
+//-----------------------------
+var num1 = 10;
+var num2 = 30;
+
+var sum = num1 + num2;
+console.log(sum);
+
+var rest = num1 - num2;
+console.log(rest);
+
+var mult = num1 * num2;
+console.log(mult);
+
+var div = num1 / num2;
+console.log(div);
+
+//-----------------------------
+// var user=prompt("Name: ");
+// alert('Welcome ' + user);
+
+var num1 = parseInt(prompt("Enter a num1: "));
+var num2 = parseInt(prompt("Enter a num2: "));
+
+var sum = num1 + num2;
+console.log(sum);
+
+var rest = num1 - num2;
+console.log(rest);
+
+var mult = num1 * num2;
+console.log(mult);
+
+var div = num1 / num2;
+console.log(div);
