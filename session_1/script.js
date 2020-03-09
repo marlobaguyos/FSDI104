88126047bc006efa41b12269ebc58c95f25e15b2
Marlo <marlo.baguyos@outlook.com>
2020-03-09 13:55:13 -0400

    update

---
+++ session_1/script.js
@@ -0,0 +1,36 @@
+//alert("hello")
+
+// prompt("Enter your name: ");
+// console.log("my first message on the console");
+
+var x = 10;
+let y = 20;
+const name="Chris";
+
+console.log(name);
+x="hello";
+y="pogi";
+console.log(x);
+console.log(y);
+
+var sentence = "Cohort 7";
+//var message = "the best cohort ever";
+
+console.log(sentence);
+sentence="the best " + sentence + " ever";
+sentence=sentence.replace('best', 'worst');
+
+console.log(sentence);
+
+var num1=10;
+var num2=30;
+
+var sum = num1 + num2;
+console.log(sum);
+var rest = num1 - num2;
+console.log(rest);
+var mult = num1 * num2;
+console.log(mult);
+var div = num1 / num2;
+console.log(div);
+
