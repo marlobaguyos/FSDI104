a040b7511895ee9e221acc7e021cc6248fa7a2d9
Marlo <marlo.baguyos@outlook.com>
2020-03-19 08:56:10 -0400

    update

--- Assignments/JavaScriptFundamentalsExercise1/exercise1.js
+++ Assignments/JavaScriptFundamentalsExercise1/exercise1.js
@@ -1,2 +1,14 @@
-var averageScore = [80, 70, 60, 90, 95, 85, 75];
-console.table(days);
+var sum = 0;
+var score = [80, 70, 60, 90, 95, 85, 75];
+
+score.forEach(myFunction);
+
+function myFunction(i) {
+    alert(i);
+    sum += i;
+}
+
+var average = sum/score.length;
+alert("Average Score is: " + average);
+console.log(average);
+
