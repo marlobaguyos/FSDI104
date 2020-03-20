c9e14549efb30952e2107ed47bbf872bd51afba4
Marlo <marlo.baguyos@outlook.com>
2020-03-20 12:06:47 -0400

    update

--- session_1/script3.js
+++ session_1/script3.js
@@ -4,7 +4,8 @@ const person1 = {
     //attributes name = value
     name: 'Samantha',
     age: 30,
-    job: 'Professor'
+    job: 'Professor',
+    music:["REM", "Coldplay", "The Beatles"]
 }
 
-console.log(person1);
+console.log(person1.music[1]);
