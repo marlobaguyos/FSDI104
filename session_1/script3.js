7dbf7c4780735d500682a99abd5e590ee4d120bc
Marlo <marlo.baguyos@outlook.com>
2020-03-20 12:44:10 -0400

    update

--- session_1/script3.js
+++ session_1/script3.js
@@ -1,11 +1,29 @@
-// object literal
+// object literal {}
 
 const person1 = {
     //attributes name = value
     name: 'Samantha',
-    age: 30,
+    age: 34,
     job: 'Professor',
-    music:["REM", "Coldplay", "The Beatles"]
+    music: ["REM", "Coldplay", "The Beatles"], //arrays []
+    address: {
+        street: 'Calz Tecnologico',
+        number: '262'
+    },
+    birthday: function () {
+        let date = new Date();
+        return date.getFullYear() - this.age;
+    }
 }
 
-console.log(person1.music[1]);
+const person2 = {
+    name: 'Sergio',
+    age: 35,
+    job: true,
+    birthday: function () {
+        let date = new Date();
+        return date.getFullYear() - this.age;
+    }
+}
+
+console.log(person1.birthday());
