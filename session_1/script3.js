16e6a26cd87f8fe51aa38a7e6d54e234ffc8c97b
Marlo <marlo.baguyos@outlook.com>
2020-03-20 14:53:11 -0400

    update

--- session_1/script3.js
+++ session_1/script3.js
@@ -28,3 +28,37 @@ const person2 = {
 
 console.log(person1.birthday());
 console.log(person2.birthday());
+
+//object contructor
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const p1 = new Person('Pavel', 25);
+const p2 = new Person('John', 28);
+const p3 = new Person('Marlo', 30);
+const p4 = new Person('Jose', 32);
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
+console.log(p4);
+
+class Tasks {
+    constructor(name, priority, date) {
+        this.name = name
+        this.priority = priority;
+        this.date = date;
+    }
+}
+
+const t1 = new Tasks('Exam', 'High', '02/19/2020');
+const t2 = new Tasks('HW','Medium', '02/18/2020');
+const t3 = new Tasks('TTH', 'low', '02/20/2020');
+
+console.log(t1);
+console.log(t2);
