12f509f28107c0681182a5e07db455f736a87b6d
Marlo <marlo.baguyos@outlook.com>
2020-03-25 16:08:04 -0400

    update

--- CR/script.js
+++ CR/script.js
@@ -1,16 +1,20 @@
 class Character {
-    constructor(name,energy,hp){
+    constructor(image,name,energy,hp,id){
         this.name=name;
         this.energy=energy;
         this.hp=hp;
+        this.id=id;
+        this.image=image
     }
     display = function () {
-        document.getElementById("redinfo").innerHTML=`Name: ${this.name} <br> Energy: ${this.energy} <br> HP:${this.hp}`;
+        document.getElementById("img" + this.id).innerHTML=`<img class="character" src="${this.image}">`;
+        document.getElementById(this.id).innerHTML=`Name: ${this.name} <br> Energy: ${this.energy} <br> HP:${this.hp}`;
 
     }
 }
 
-const c1 = new Character("Red",100,10);
-const c2 = new Character("Yellow",100,8);
+const c1 = new Character("img/pngguru.com (3).png","Red",100,10,"redinfo");
+const c2 = new Character("img/pngguru.com (2).png","Yellow",100,8,"yellowinfo");
 
-c1.display(redinfo);
+c1.display();
+c2.display();
