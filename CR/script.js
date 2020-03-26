2b951eae7c54117fd1f15fb985d83bedcb2fde09
Marlo <marlo.baguyos@outlook.com>
2020-03-25 20:12:42 -0400

    update

--- CR/script.js
+++ CR/script.js
@@ -11,16 +11,29 @@ class Character {
         document.getElementById(this.id).innerHTML=`Name: ${this.name} <br> Energy: ${this.energy} <br> HP:${this.hp}`;
     }
 
-    attack = function(opponent){
-        var newEnergy = opponent.energy - this.hp;
+    attack = function(opponent,item){
+        var newEnergy = opponent.energy - this.imagic;
         opponent.energy=newEnergy;
         console.log(opponent.energy);
         opponent.display();
     }
 }
 
-const c1 = new Character("img/pngguru.com (3).png","Red",100,10,"redinfo");
-const c2 = new Character("img/pngguru.com (2).png","Yellow",100,8,"yellowinfo");
+const c1 = new Character("img/pngguru.com (3).png","Red",100,0,"redinfo");
+const c2 = new Character("img/pngguru.com (2).png","Yellow",100,0,"yellowinfo");
 
 c1.display();
 c2.display();
+
+class Item{
+    constructor(iname,imagic,img){
+        this.iname=iname;
+        this.imagic=imagic;
+        this.img=img;
+    }
+}
+
+const item1 = new Item("vodka",30,'img/vodka.png');
+const item2 = new Item("beer",15,'img/beer.png');
+const item3 = new Item("tequila",20,'img/tequila.png');
+
